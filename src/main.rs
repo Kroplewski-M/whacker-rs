c5b2c2cc441f96aad2797d15bf95b381b61de5da
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2026-06-20 21:06:32 +0100

    simple hypr client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use http_body_util::{BodyExt, Empty};
+use hyper::{Request, body::Bytes};
+use hyper_util::rt::TokioIo;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let url = "http://httpbin.org/ip".parse::<hyper::Uri>()?;
+
+    let host = url.host().expect("uri has no host");
+    let port = url.port_u16().unwrap_or(80);
+
+    let address = format!("{}:{}", host, port);
+
+    //open TCP connection
+    let stream = TcpStream::connect(address).await?;
+
+    // Use an adapter to access something implementing `tokio::io` traits as if they implement
+    let io = TokioIo::new(stream);
+
+    //create hypr client
+    let (mut sender, conn) = hyper::client::conn::http1::handshake::<_, Empty<Bytes>>(io).await?;
+
+    // Spawn a task to poll the connection, driving the HTTP state
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    // The authority of our URL will be the hostname of the httpbin remote
+    let authority = url.authority().unwrap().clone();
+
+    //Create an Http request with an empty body and a HOST header
+    let req = Request::builder()
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    //await response
+    let mut res = sender.send_request(req).await?;
+    println!("Response status: {}", res.status());
+
+    // Stream the body, writing each frame to stdout as it arrives
+    while let Some(next) = res.frame().await {
+        let frame = next?;
+        if let Some(chunk) = frame.data_ref() {
+            tokio::io::stdout().write_all(chunk).await?;
+        }
+    }
+    Ok(())
 }
